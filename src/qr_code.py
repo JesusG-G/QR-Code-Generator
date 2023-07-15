ec70d232628d3c85423ecfb6c3c16d5e3bdd74d9
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-07-14 22:41:26 -0400

    [JESUS] - change in the function generate_qr, now return a boolean value

--- src/qr_code.py
+++ src/qr_code.py
@@ -3,15 +3,14 @@ import qrcode
 class QrCodeGenerator:
     def __init__(self, size:int, padding: int) -> None:
         self.qr = qrcode.QRCode(box_size=size, border=padding)
-    
-    def generate_qr(self, file_name:str,data_qr: str, fill_color: str, background_color: str):
+    def generate_qr(self, file_name:str,data_qr: str, fill_color: str, background_color: str) -> bool:
         try:
             self.qr.add_data(data_qr)
             qr_image = self.qr.make_image(fill_color= fill_color,back_color= background_color).convert('RGB')
             qr_image.save(file_name)
-            print(f'Successfully created {file_name}')
+            return True
         except Exception as e:
-            print(f"Error: {e}")
+            return e
     def saveQr(self):
         self.qr.save
     
