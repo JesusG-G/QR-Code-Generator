662ef26f7ec439bb5159920e193935388e8fe993
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-07-20 15:09:26 -0400

    [JESUS] - Update the generate_qr function and create the function to save the qr

--- src/qr_code.py
+++ src/qr_code.py
@@ -4,28 +4,29 @@ from PIL import Image
 class QrCodeGenerator:
     def __init__(self) -> None:
             self.qr = qrcode.QRCode(version=1, error_correction=qrcode.ERROR_CORRECT_H)
-    def generate_qr(self, file_name:str,data_qr: str,image_path: str, fill_color: str, background_color: str) -> bool:
+    def generate_qr(self,path_to_save:str,data_qr: str,image_path: str) -> bool:
         try:
-            if image_path and ('.png' in image_path  or '.jpg' in image_path):
+            if image_path and '.png' in image_path:
                 logo = Image.open(image_path)
                 print(logo)
                 #Ajust the size of the image
                 hsize = int(float(logo.size[1])*float(100/float(logo.size[0])))
                 logo = logo.resize((100,hsize), Image.Resampling.LANCZOS)
                 self.qr.add_data(data_qr)
-                qr_image = self.qr.make_image(fill_color= fill_color,back_color= background_color).convert('RGB')
+                self.qr_image = self.qr.make_image(fill_color='black', background_color='white').convert('RGB')
                 #Position of the image
-                position = ((qr_image.size[0] - logo.size[0]) // 2, (qr_image.size[1] - logo.size[1]) // 2)
-                qr_image.paste(logo,position)
-                qr_image.save(file_name)
+                position = ((self.qr_image.size[0] - logo.size[0]) // 2, (self.qr_image.size[1] - logo.size[1]) // 2)
+                self.qr_image.paste(logo,position)
+                self.saveQr(path_to_save)
                 return True
             elif not image_path:
                 self.qr.add_data(data_qr)
-                qr_image = self.qr.make_image(fill_color= fill_color,back_color= background_color).convert('RGB')
-                qr_image.save(file_name)
+                self.qr_image = self.qr.make_image(fill_color='black', background_color='white').convert('RGB')
+                self.saveQr(path_to_save)
                 return True
         except Exception as e:
             return e
-    def saveQr(self):
-        self.qr.save
+    def saveQr(self, path_save):
+        full_path = f"{path_save}.png"
+        self.qr_image.save(full_path)
     
