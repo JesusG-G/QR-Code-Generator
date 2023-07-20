1f920f8123afd06068b5ca4eb774a497a5e1562d
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-07-20 13:42:37 -0400

    [JESUS] - Popup and popup.kv files created with the logic of the popups

---
+++ src/shared/popup/popup.py
@@ -0,0 +1,14 @@
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
+
+class SuccessPopup(Popup):
+    pass
+
+class FailPopup(Popup):
+    pass
+
+class MessageErrorPopup(Popup):
+    pass
+
+class FileErrorPopup(Popup):
+    pass
